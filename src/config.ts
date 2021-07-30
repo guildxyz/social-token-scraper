9be092c9a029c908585ac440af3ec69598e4f547
DevidAbt <abt.devid@gmail.com>
2021-07-30 10:00:58 +0200

    Added ethers and dotenv

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,21 @@
+import dotenv from "dotenv";
+
+const envFound = dotenv.config();
+if (envFound.error)
+  throw new Error(
+    `Couldn't find .env file or volumes in compose. ${envFound.error}`
+  );
+
+const etherscanApiKey = process.env.ETHERSCAN_API_KEY;
+if (!etherscanApiKey) {
+  throw new Error(
+    "You need to specify the bot's DISCORD_TOKEN in the .env file."
+  );
+}
+
 const coingeckoBaseUrl = "https://api.coingecko.com/api/v3";
 
 export default {
   coingeckoBaseUrl,
+  etherscanApiKey,
 };
